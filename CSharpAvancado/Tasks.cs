10143d39c5d2e3148eaebc853e1531b1f8c7b50d
carlos.silva <carlos.silva@indigosoft.com.br>
2019-11-26 17:16:40 -0300

    initial Tasks

--- CSharpAvancado/Tasks.cs
+++ CSharpAvancado/Tasks.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace CSharpAvancado
 {
-    class Tasks
+ 
+
+    public class Tasks
     {
+        private static void DoJob()
+        {
+            for (int i = 0; i < 100; i++)
+                Console.WriteLine(i);
+        }
+
+        private static double Dobro(double x)
+        {
+            return 2 * x;
+        }
+
+        static void Main(string[] args)
+        {
+            // Tasks alike Threads
+            Task t = new Task(DoJob);
+            t.Start();
+
+            Task t1 = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                    Console.WriteLine(i);
+            });
+
+            Task.Run(() => DoJob());
+            Task.Factory.StartNew(DoJob);
+
+            //Task<Double> dobro = new Task<double>(Dobro(10.2));
+            Task<double> dobro = Task.Run(() => Dobro(15.0));
+        }
+
     }
 }
