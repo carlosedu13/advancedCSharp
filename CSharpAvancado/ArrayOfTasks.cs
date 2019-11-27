f180db4511adf07f44ee662a6b15e36495815b16
carlosedu13 <carlosedu.waldorf@gmail.com>
2019-11-27 12:33:03 -0300

    add Tasks Array and Task ContinueWith

---
+++ CSharpAvancado/ArrayOfTasks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAvancado
+{
+    class ArrayOfTasks
+    {
+        public static void Main(string[] args)
+        {
+            Task[] tasks = new Task[2]
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Task1");
+                }),
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Task2");
+                }),
+            };
+
+            Task.WaitAll(tasks);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpAvancado/TasksWithContinue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAvancado
+{
+    class TasksWithContinue
+    {
+        public static void Main(string[] args)
+        {
+            Task<int> passo1 = Task.Factory.StartNew(() =>
+            {
+                Random r = new Random();
+                return (int)r.Next(100);
+            });
+
+            Task<int> passo2 = passo1.ContinueWith(numero => numero.Result * 2);
+            Task<int> passo3 = passo2.ContinueWith(numero => numero.Result * 3);
+
+            Console.WriteLine(passo3.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
