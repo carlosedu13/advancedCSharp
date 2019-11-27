d7e9626363e276c3e9390d80e80196b752d2799f
carlosedu13 <carlosedu.waldorf@gmail.com>
2019-11-27 13:20:54 -0300

    async/await

---
+++ CSharpAvancado/AsyncAwait.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAvancado
+{
+    class AsyncAwait
+    {
+        public static void Main(string[] args)
+        {
+            PrintMessage("John the ripper");
+
+            Console.ReadKey();
+        }
+
+        static async void PrintMessage(string name)
+        {
+            Console.WriteLine("{0}", await Task.Run(() => GetMessage(name)));
+        }
+        
+        static string GetMessage(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
